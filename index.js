2ef1576ff209c58c7c782c96c5d4bc2004ca2d4b
MrWilliamA <will.archer@hotmail.com>
2022-03-01 17:44:35 +1100

    fin

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+
+const writeCards = (name, event) => {
+
+    let newName = [];
+    for(let i = 0; i < name.length; i++) {
+        newName.push("the current name is " + name[i]);   
+ 
+    }
+    return newName;
+}
+
+writeCards(["Ada", "Brendan", "Ali"], "birthday"); 
+
+
+const countDown = (num) => {
+ counter = 0;
+
+ while(counter < num) {
+console.log(num);
+num--;
+ }
+
+}
